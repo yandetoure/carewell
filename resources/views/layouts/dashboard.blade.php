37ff29f13cb7ec2f79bc1e85ce1f576ba1c1c6e1
yandetoure <ndeyeyandemouhammad@gmail.com>
2025-10-18 14:19:13 +0000

    Mise a jour de la taille de ppolice

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -8,6 +8,8 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom CSS -->
+    @vite(['resources/css/app.css'])
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <!-- Google Fonts -->
